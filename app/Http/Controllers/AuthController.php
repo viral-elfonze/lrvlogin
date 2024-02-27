314018d06a5e24ca85f6b51c0979bb543ef0b358
viral champanery <vchampanery@gmail.com>
2024-02-27 12:40:34 +0530

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Microsoft\Graph\Graph as MicrosoftGraph;
 use Microsoft\Graph\Model;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -29,7 +30,7 @@ class AuthController extends Controller
     public function handleMicrosoftCallback(Request $request)
     {
 
-        dump($request);
+        // dump($request);
         $guzzle = new \GuzzleHttp\Client();
         $provider = new \TheNetworg\OAuth2\Client\Provider\Azure([
             'clientId' => env('MS_GRAPH_CLIENT_ID'),
@@ -39,7 +40,7 @@ class AuthController extends Controller
 
         // Validate state
         $state = $request->session()->pull('oauth2state');
-        dump($state);
+        // dump($state);
         // dd();
         // if (empty($state) || ($state !== $request->input('state'))) {
         //     return redirect('/');
@@ -50,19 +51,43 @@ class AuthController extends Controller
             'code' => $request->input('code'),
         ]);
 
-        dump($token);
-        dump($token->getValues());
+        // dump($token);
+        // dump($token->getValues());
 
-        dump($token->getToken());
+        // dump($token->getToken());
         $resourceOwner = $provider->getResourceOwner($token);
-        dump($resourceOwner->getUpn());
-        dd("test");
-        // Use Microsoft Graph SDK to interact with Microsoft Graph
-        $graph = new Graph();
-        $graph->setAccessToken($token->getToken());
-        $me = $graph->createRequest('GET', '/me')->setReturnType(Model\User::class)->execute();
-        dump($me->getDisplayName());
-        dd($me);
-        // Use $me->getDisplayName() or other properties for user viinfo
+        $email = $resourceOwner->getUpn();
+        $name = $resourceOwner->getName();
+
+        //create the user if not exists
+        $user = User::where('email', $email)->first();
+        if(!$user){
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make($request->password)
+            ]);
+        }
+        $data['token'] = $user->createToken($email)->plainTextToken;
+
+        auth()->login($user);
+        $data['user'] = $user;
+
+        $response = [
+            'status' => 'success',
+            'message' => 'User is logged in successfully.',
+            'data' => $data,
+        ];
+
+        return response()->json($response, 200);
+
+        // dd("test");
+        // // Use Microsoft Graph SDK to interact with Microsoft Graph
+        // $graph = new Graph();
+        // $graph->setAccessToken($token->getToken());
+        // $me = $graph->createRequest('GET', '/me')->setReturnType(Model\User::class)->execute();
+        // dump($me->getDisplayName());
+        // dd($me);
+        // // Use $me->getDisplayName() or other properties for user viinfo
     }
 }
