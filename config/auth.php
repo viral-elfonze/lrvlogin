9fd2ffcd830380029e87b6c64b5f80c9116112a7
viral champanery <vchampanery@gmail.com>
2024-03-05 14:19:39 +0530

    update the auth

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'sanctum', // or any other driver like 'sanctum' or 'jwt'
+            'provider' => 'users',
+        ],
     ],
 
     /*
