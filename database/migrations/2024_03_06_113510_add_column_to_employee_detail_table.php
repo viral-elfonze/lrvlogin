ee3d64144f66dfa7a2e23d15b0b98ba02d84214b
Dixita Mistra <dixita.mistra@ihsmarkit.com>
2024-03-06 17:12:57 +0530

    Migration for employee details

---
+++ database/migrations/2024_03_06_113510_add_column_to_employee_detail_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employee_detail', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+
+            // Define foreign key constraint
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('NO ACTION')
+                ->onDelete('NO ACTION');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->longText('api_token')->after('remeber_token')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employee_detail', function (Blueprint $table) {
+            //
+        });
+    }
+};
