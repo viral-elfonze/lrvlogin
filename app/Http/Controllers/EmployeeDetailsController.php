90f4c07049dcb80c632dde7109b449bfef618330
dixita.mistra <dixita.mistra@ihsmarkit.com>
2024-03-12 10:24:34 +0530

    Delete Employee Details Fixes

--- app/Http/Controllers/EmployeeDetailsController.php
+++ app/Http/Controllers/EmployeeDetailsController.php
@@ -27,7 +27,7 @@ class EmployeeDetailsController extends Controller
     public function getEmployeeDetails()
     {
         try {
-            $employeesData = EmployeeDetails::all();
+            $employeesData = EmployeeDetails::where('deleted_at', null)->get();
 
             //If employee data not found
             if (!$employeesData) {
@@ -137,7 +137,6 @@ class EmployeeDetailsController extends Controller
                 }
 
                 $employee = new EmployeeDetails();
-
                 $employee->user_id = auth()->user()->id;
                 $employee->employee_firstname = $request->input('employee_firstname');
                 $employee->employee_middlename = $request->input('employee_middlename');
@@ -172,7 +171,7 @@ class EmployeeDetailsController extends Controller
     {
         try {
             // Find the employee record by ID
-            $employeeDetails = EmployeeDetails::with('imageMaster')->where('user_id', $id)->get();
+            $employeeDetails = EmployeeDetails::where('user_id', $id)->get();
 
 
             if (!$employeeDetails) {
