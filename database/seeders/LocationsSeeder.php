abe976e8efb9231779fe4d15bd4adc9261354ef1
dixita.mistra <dixita.mistra@ihsmarkit.com>
2024-03-15 12:10:29 +0530

    Location seeder updated

--- database/seeders/LocationsSeeder.php
+++ database/seeders/LocationsSeeder.php
@@ -13,10 +13,40 @@ class LocationsSeeder extends Seeder
      */
     public function run(): void
     {
-        Locations::create(['city' => 'Ahmedabad']);
-        Locations::create(['city' => 'Anand']);
-        Locations::create(['city' => 'Bangalore']);
-        Locations::create(['city' => 'Surat']);
-        Locations::create(['city' => 'Rajkot']);
+        Locations::create(['city' => 'Mumbai']);
+        Locations::create(['city' => 'Delhi']);
+        Locations::create(['city' => 'Kolkata']);
+        Locations::create(['city' => 'Chennai']);
+        Locations::create(['city' => 'Hyderabad']);
+        Locations::create(['city' => 'Pune']);
+        Locations::create(['city' => 'Jaipur']);
+        Locations::create(['city' => 'Lucknow']);
+        Locations::create(['city' => 'Kanpur']);
+        Locations::create(['city' => 'Nagpur']);
+        Locations::create(['city' => 'Visakhapatnam']);
+        Locations::create(['city' => 'Indore']);
+        Locations::create(['city' => 'Thane']);
+        Locations::create(['city' => 'Bhopal']);
+        Locations::create(['city' => 'Patna']);
+        Locations::create(['city' => 'Vadodara']);
+        Locations::create(['city' => 'Ghaziabad']);
+        Locations::create(['city' => 'Ludhiana']);
+        Locations::create(['city' => 'Agra']);
+        Locations::create(['city' => 'Nashik']);
+        Locations::create(['city' => 'Faridabad']);
+        Locations::create(['city' => 'Meerut']);
+        Locations::create(['city' => 'Varanasi']);
+        Locations::create(['city' => 'Srinagar']);
+        Locations::create(['city' => 'Kochi']);
+        Locations::create(['city' => 'Coimbatore']);
+        Locations::create(['city' => 'Madurai']);
+        Locations::create(['city' => 'Vijayawada']);
+        Locations::create(['city' => 'Mysore']);
+        Locations::create(['city' => 'Tiruchirappalli']);
+        Locations::create(['city' => 'Guwahati']);
+        Locations::create(['city' => 'Jodhpur']);
+        Locations::create(['city' => 'Amritsar']);
+        Locations::create(['city' => 'Shimla']);
+        Locations::create(['city' => 'Gangtok']);
     }
 }
