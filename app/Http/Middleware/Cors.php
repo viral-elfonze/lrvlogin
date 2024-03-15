dd9d925f6212375f4c09e5aa4c1719eeeb6c5623
viral champanery <vchampanery@gmail.com>
2024-03-15 19:23:49 +0530

    update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,6 +18,7 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
             ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Credentials', true)
             ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
             ->header('Access-Control-Allow-Credentials',' true');
     }
