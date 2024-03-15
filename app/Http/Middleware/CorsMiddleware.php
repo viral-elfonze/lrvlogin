607a791aff824d11fd645b646f5aff6bb9a5ef2a
viral champanery <vchampanery@gmail.com>
2024-03-15 19:30:40 +0530

    udpate the cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,7 +8,11 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Origin', ['*','http://localhost:3000/']);
+        $response->header('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']);
+        $response->header('Access-Control-Allow-Credentials', true);
+        $response->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With');
+        $response->header('Access-Control-Allow-Credentials',' true');;
         return $response;
     }
 }
--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*','http://localhost:3000/'],
 
     'allowed_origins_patterns' => [],
 
