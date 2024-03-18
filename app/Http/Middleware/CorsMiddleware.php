0520baf82e0091f8bd5da9108595299481e261fe
dixita.mistra <dixita.mistra@ihsmarkit.com>
2024-03-18 15:07:06 +0530

    cors error solution in delete method

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Middleware;
 
 use Closure;
@@ -9,11 +10,19 @@ class CorsMiddleware
     {
         $response = $next($request);
         $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Origin', ['*','http://localhost:3000/']);
+        $response->header('Access-Control-Allow-Origin', ['*', 'http://localhost:3000/']);
         $response->header('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']);
         $response->header('Access-Control-Allow-Credentials', true);
-        $response->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With');
-        $response->header('Access-Control-Allow-Credentials',' true');
+        $response->header('Access-Control-Allow-Headers', ' Origin, Content-Type, Accept, Authorization, X-Request-With');
+        $response->header('Access-Control-Allow-Credentials', ' true');
+
+        // Handle preflight OPTIONS request
+        if ($request->isMethod('OPTIONS')) {
+            $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Requested-With');
+            return $response;
+        }
+
         return $response;
     }
 }
