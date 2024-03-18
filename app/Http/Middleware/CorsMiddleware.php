c4bbf37be8a748353d9fd1ceeb21bec11a5239a1
viral champanery <vchampanery@gmail.com>
2024-03-18 14:00:48 +0530

    update

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -9,7 +9,7 @@ class CorsMiddleware
     {
         $response = $next($request);
         $response->header('Access-Control-Allow-Origin', ['*','http://localhost:3000/']);
-        $response->header('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']);
+        $response->header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
         $response->header('Access-Control-Allow-Credentials', true);
         $response->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With');
         $response->header('Access-Control-Allow-Credentials',' true');;
