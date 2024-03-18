e8ca900f8ff733d7ed7d75b9d75d3a03721fca57
viral champanery <vchampanery@gmail.com>
2024-03-18 12:24:57 +0530

    update the cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,11 +16,8 @@ class Cors
     public function handle(Request $request, Closure $next): Response
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', ['*','http://localhost:3000/'])
-            ->header('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
-            ->header('Access-Control-Allow-Credentials', true)
-            ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
-            ->header('Access-Control-Allow-Credentials',' true');
+        ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
 
     }
